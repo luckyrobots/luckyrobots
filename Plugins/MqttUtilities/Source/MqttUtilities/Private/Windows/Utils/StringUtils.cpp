ba4cad5c862d7457070255b17fd4ebd012ac2a17
T4lus <jeremy.rigaud@live.fr>
2023-11-07 14:56:43 +0100

    MQTT Base

---
+++ Plugins/MqttUtilities/Source/MqttUtilities/Private/Windows/Utils/StringUtils.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2023 T4lus Development
+
+#include "StringUtils.h"
+
+char* StringUtils::CopyString(FString str)
+{
+	auto StringCaster = StringCast<ANSICHAR>(static_cast<const TCHAR*>(*str));
+	const char* originalStr = StringCaster.Get();
+	
+	char *copyStr;
+	size_t str_len;
+
+	if (!originalStr)
+	{
+		return nullptr;
+	}
+	
+	str_len = strlen(originalStr) + 1;
+	
+	if (nullptr == (copyStr = (char*)malloc(str_len)))
+	{
+		return nullptr;
+	}	
+	
+	memcpy(copyStr, originalStr, str_len);
+
+	return copyStr;
+}
