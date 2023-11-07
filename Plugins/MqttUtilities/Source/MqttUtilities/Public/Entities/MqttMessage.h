63e5f8cbb39b435a6909f79eb24491deff354339
T4lus <jeremy.rigaud@live.fr>
2023-11-07 14:56:43 +0100

    MQTT Base

---
+++ Plugins/MqttUtilities/Source/MqttUtilities/Public/Entities/MqttMessage.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2023 T4lus Development
+
+#pragma once
+
+#include "MqttMessage.generated.h"
+
+USTRUCT(BlueprintType)
+struct MQTTUTILITIES_API FMqttMessage
+{
+	GENERATED_BODY()
+
+	/** Message content. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MQTT")
+	FString Message;
+
+	/** Message content buffer. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MQTT")
+	TArray<uint8> MessageBuffer;
+
+	/** Message topic. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MQTT")
+	FString Topic;
+
+	/** Retain flag. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MQTT")
+	bool Retain;
+
+	/** Quality of signal. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MQTT")
+	int Qos;
+};
---
+++ Plugins/MqttUtilities/Source/MqttUtilities/Public/MqttUtilitiesBPL.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2023 T4lus Development
+
+#pragma once
+
+#include "Interface/MqttClientInterface.h"
+#include "Entities/MqttClientConfig.h"
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+
+#include "MqttUtilitiesBPL.generated.h"
+
+UCLASS()
+class MQTTUTILITIES_API UMqttUtilitiesBPL : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	/**
+	 * Create new instance of MQTT client
+	 *
+	 * @return - reference to MQTT client interface object
+	 */
+public:
+	UFUNCTION(BlueprintCallable, Category = "MQTT")
+	static TScriptInterface<IMqttClientInterface> CreateMqttClient(FMqttClientConfig config);
+};
