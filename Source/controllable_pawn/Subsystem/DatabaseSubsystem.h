ad3f2e8fd98dac2fd5dc37cfb3626fdc3b6f20fc
T4lus <jeremy.rigaud@live.fr>
2023-11-15 18:12:01 +0100

    Fixing Project

--- Source/controllable_pawn/Subsystem/DatabaseSubsystem.h
+++ Source/controllable_pawn/Subsystem/DatabaseSubsystem.h
@@ -8,9 +8,25 @@
 
 #include "DatabaseSubsystem.generated.h"
 
-/**
- * 
- */
+
+USTRUCT(BlueprintType)
+struct FDatabaseMovements
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Command;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Scale;
+
+	FDatabaseMovements(FString _Command = "", float _Scale = 0.f)
+	{
+		Command = _Command;
+		Scale = _Scale;
+	}
+};
+
 UCLASS()
 class CONTROLLABLE_PAWN_API UDatabaseSubsystem : public UGameInstanceSubsystem
 {
@@ -25,7 +41,15 @@ public:
 
 	bool SaveScreenshot(TArrayView<const uint8> LeftCameraData, TArrayView<const uint8> RightCameraData);
 
+	UFUNCTION(BlueprintCallable)
+	FDatabaseMovements GetLastMovement();
+
 private:
-	FSQLiteDatabase* Database;
+	FSQLiteDatabase* OutputDatabase;
+	FSQLiteDatabase* InputDatabase;
+
+private:
+	int32 ScreenshotCount;
+	int32 CurrentScreenshot;
 
 };
--- Source/controllable_pawn/controllable_pawn.Build.cs
+++ Source/controllable_pawn/controllable_pawn.Build.cs
@@ -19,17 +19,18 @@ public class controllable_pawn : ModuleRules
             }
         );
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-        PublicDependencyModuleNames.AddRange(new string[] { "DatabaseSupport", "SQLiteCore", "SQLiteSupport" });
+        PublicDependencyModuleNames.AddRange(new string[] { 
+            "Core", 
+            "CoreUObject", 
+            "Engine", 
+            "InputCore" 
+        });
 
-        PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PublicDependencyModuleNames.AddRange(new string[] { 
+            "DatabaseSupport", 
+            "SQLiteCore", 
+            "SQLiteSupport" 
+        });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+    }
 }
