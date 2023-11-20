ca3ef695c38d1f98e477e1ec95a921353555c7e8
Devrim Yasar <dvrmysr@gmail.com>
2023-11-20 15:17:23 -0600

    removed the need to visual studio build step

--- Source/controllable_pawn/controllable_pawn.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "controllable_pawn.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, controllable_pawn, "controllable_pawn" );
