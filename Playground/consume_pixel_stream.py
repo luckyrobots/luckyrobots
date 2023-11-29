bc2f13eee930959a38b35203f6c50ff8daed02f9
gurkan01 <gcorum01@gmail.com>
2023-11-29 20:53:30 +0300

    Dvr (#15)
    * tiny changes now going to add redis back
    
    * streamers added
    
    * pixelstreaming is implemented on the new lucky robot
    
    * trying to figure out cirrus.js
    
    ---------
    
    Co-authored-by: Devrim Yasar <dvrmysr@gmail.com>

---
+++ Playground/consume_pixel_stream.py
@@ -0,0 +1,53 @@
+import asyncio
+import cv2
+import numpy as np
+from aiortc import RTCPeerConnection, VideoStreamTrack
+from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder
+
+class VideoImageTrack(VideoStreamTrack):
+    """
+    A video track that returns an image received from the WebRTC stream.
+    """
+    def __init__(self, track):
+        super().__init__()  # don't forget this!
+        self.track = track
+
+    async def recv(self):
+        frame = await self.track.recv()
+
+        # convert PIL image to OpenCV format
+        img = cv2.cvtColor(np.array(frame.to_pil()), cv2.COLOR_RGB2BGR)
+
+        # process image with OpenCV
+        img = cv2.Canny(img, 100, 200)
+
+        # return modified image
+        return img
+
+async def run(pc):
+    def add_tracks():
+        if player and player.video:
+            pc.addTrack(VideoImageTrack(player.video))
+
+    @pc.on("track")
+    def on_track(track):
+        print("Track %s received" % track.kind)
+        if track.kind == "video":
+            local_video = VideoImageTrack(track)
+            pc.addTrack(local_video)
+
+    player = MediaPlayer('ws://localhost:8888')
+    add_tracks()
+
+    await pc.setLocalDescription(await pc.createOffer())
+    await asyncio.sleep(1)
+
+if __name__ == "__main__":
+    pc = RTCPeerConnection()
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(run(pc))
+    except Exception as e:
+        print(e)
+    finally:
+        loop.run_until_complete(pc.close())
---
+++ Plugins/RedisConnector/Source/DTRedisLib/Private/alloc.c
@@ -0,0 +1,59 @@
+﻿// Copyright 2022 Dexter.Wan. All Rights Reserved. 
+// EMail: 45141961@qq.com
+
+#include "fmacros.h"
+#include "alloc.h"
+#include <string.h>
+#include <stdlib.h>
+
+hiredisAllocFuncs hiredisAllocFns = {
+    .mallocFn = malloc,
+    .callocFn = calloc,
+    .reallocFn = realloc,
+    .strdupFn = strdup,
+    .freeFn = free,
+};
+
+/* Override hiredis' allocators with ones supplied by the user */
+hiredisAllocFuncs hiredisSetAllocators(hiredisAllocFuncs *override) {
+    hiredisAllocFuncs orig = hiredisAllocFns;
+
+    hiredisAllocFns = *override;
+
+    return orig;
+}
+
+/* Reset allocators to use libc defaults */
+void hiredisResetAllocators(void) {
+    hiredisAllocFns = (hiredisAllocFuncs) {
+        .mallocFn = malloc,
+        .callocFn = calloc,
+        .reallocFn = realloc,
+        .strdupFn = strdup,
+        .freeFn = free,
+    };
+}
+
+#ifdef _WIN32
+
+void *hi_malloc(size_t size) {
+    return hiredisAllocFns.mallocFn(size);
+}
+
+void *hi_calloc(size_t nmemb, size_t size) {
+    return hiredisAllocFns.callocFn(nmemb, size);
+}
+
+void *hi_realloc(void *ptr, size_t size) {
+    return hiredisAllocFns.reallocFn(ptr, size);
+}
+
+char *hi_strdup(const char *str) {
+    return hiredisAllocFns.strdupFn(str);
+}
+
+void hi_free(void *ptr) {
+    hiredisAllocFns.freeFn(ptr);
+}
+
+#endif
