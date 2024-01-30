e1925d2ce799274cc4cd651c5be06767e6e7dcdf
Devrim Yasar <dvrmysr@gmail.com>
2024-01-30 14:20:09 -0600

    add Resources back

---
+++ Resources/SignallingWebServer/Public/login.js
@@ -0,0 +1,1 @@
+!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports={}:"function"==typeof define&&define.amd?define([],o):"object"==typeof exports?exports["epicgames-frontend"]={}:e["epicgames-frontend"]={}}(this,(()=>({})));
