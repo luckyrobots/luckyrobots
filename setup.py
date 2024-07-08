05d84439544719c6d791a27dcf537507fe39b68b
Devrim Yasar <dvrmysr@gmail.com>
2024-07-08 14:55:52 -0500

    working version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="luckyrobots",
-    version="0.1.15",
+    version="0.1.16",
     description="Robotics-AI Training in Hyperrealistic Game Environments",
     long_description=long_description,
     long_description_content_type="text/markdown",
